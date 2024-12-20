b6a2f85aba1da11d811decbea32e4582b4361db1
devSurfer <2929408642@qq.com>
2024-12-20 23:00:11 +0800

    新功能(util): 添加日志记录功能 - 记录请求响应json转换过程的日志输出

--- src/main/java/com/surfer/service/util/WebUtil.java
+++ src/main/java/com/surfer/service/util/WebUtil.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.surfer.service.common.Result;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,7 +19,9 @@ import java.text.SimpleDateFormat;
  */
 public class WebUtil {
 
-    private static ObjectMapper objectMapper;
+    private static final Logger log = LoggerFactory.getLogger(WebUtil.class);
+
+    private static final ObjectMapper objectMapper;
 
     static {
         objectMapper = new ObjectMapper();
@@ -27,14 +31,18 @@ public class WebUtil {
 
     // 从请求中获取json串转为object
     public static <T> T readJSON(HttpServletRequest request, Class<T> clazz) {
+        log.info("转换对象: {}", clazz.getName());
         if (request == null) return null;
         try {
             BufferedReader reader = request.getReader();
             StringBuffer buffer = new StringBuffer();
             String line = null;
+            log.info("读取的行内容：");
             while ((line = reader.readLine()) != null) {
+                log.info(line);
                 buffer.append(line);
             }
+            log.info("转换完成，对象内容：{}", buffer);
             return objectMapper.readValue(buffer.toString(), clazz);
         } catch (IOException e) {
             throw new RuntimeException("[readJSON] 解析请求体json为对象异常");
@@ -43,10 +51,13 @@ public class WebUtil {
 
     // 将object转为json串并放到请求体中
     public static void writeJSON(HttpServletResponse response, Result<?> result) {
+        log.info("响应对象：{}", result);
         if (result == null) return;
         try {
-            String resultJson = objectMapper.writeValueAsString(response);
+            log.info("响应，开始转换");
+            String resultJson = objectMapper.writeValueAsString(result);
             response.getWriter().write(resultJson);
+            log.info("响应，转换完成：{}", resultJson);
         } catch (Exception e) {
             throw new RuntimeException("[writeJSON] 实体类转为响应体json异常");
         }
