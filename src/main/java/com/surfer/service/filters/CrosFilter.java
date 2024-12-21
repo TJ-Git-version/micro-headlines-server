eeafeeb5a14204faad763c72f565f698a9ecc67a
devSurfer <2929408642@qq.com>
2024-12-21 13:48:51 +0800

    新功能(filters): 封装跨域过滤器方式解决跨域问题

---
+++ src/main/java/com/surfer/service/filters/CrosFilter.java
@@ -0,0 +1,32 @@
+package com.surfer.service.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * @author Dev Surfer
+ * @version 1.0.0
+ * date 2024/12/21 13:37
+ * description 服务端解决跨域请求,实现filter
+ */
+public class CrosFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        // 允许的请求来源  *（允许所有来源）
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        // 允许的请求方式
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        // 允许携带的请求头
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        // 是否允许携带cookie
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        // 预检请求的缓存时间（秒）
+        response.setHeader("Access-Control-Max-Age", "3600");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+}
